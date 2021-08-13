0e2078a4a209aa040be143ff58c639ab82adeb54
juan-miguel-gomez <54858894+juan-miguel-gomez@users.noreply.github.com>
2021-08-13 00:46:58 -0500

    Update settings.py

--- social_django/social_django/settings.py
+++ social_django/social_django/settings.py
@@ -16,7 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+STATIC_ROOT = BASEPATH.joinpath('static')
+MEDIA_ROOT = BASEPATH.joinpath('media')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
