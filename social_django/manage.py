77b5089a5522c19cf2194b7ed0a9258127ce7ef2
Leonardo Daniel Ramirez Reyes <gomez.olvera.juan.miguel@gmail.com>
2021-08-13 02:20:13 -0500

    Segunda version estable

--- social_django/manage.py
+++ social_django/manage.py
@@ -3,21 +3,6 @@
 import os
 import sys
 
-<!-- Web.Config Configuration File -->
-
-<configuration>
-    <system.web>
-        <customErrors mode="Off"/>
-    </system.web>
-</configuration>
-
-<!-- Web.Config Configuration File -->
-
-<configuration>
-    <system.web>
-        <customErrors mode="RemoteOnly" defaultRedirect="mycustompage.htm"/>
-    </system.web>
-</configuration>
 
 def main():
     """Run administrative tasks."""
